24021d067e58638c8466a25d23ae9d42608fd678
Saurabh Datta <hi@dattasaurabh.com>
2024-09-16 18:56:21 +0200

    trying blinka

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-import RPi.GPIO as GPIO
 from time import sleep
+import board
+import digitalio
 
-WIFI_RESET_PIN = 23
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-def button_pressed(WIFI_RESET_PIN):
-    print("Button pressed.")
-
-GPIO.setup(WIFI_RESET_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(WIFI_RESET_PIN, GPIO.RISING, callback=button_pressed, bouncetime=10) # add rising edge detection on a channel
+print("press the button!")
 
 
-try:
-    while True:
-        sleep(10)
-finally:
-    GPIO.cleanup()
+button = digitalio.DigitalInOut(board.D4)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+
+while True:
+    print(button.value)
+    sleep(2)
