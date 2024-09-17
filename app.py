db77e0d3566e2edc3af1fab72653b78585f83b91
Saurabh Datta <hi@dattasaurabh.com>
2024-09-17 14:17:31 +0200

    updating TBD list

--- app.py
+++ app.py
@@ -3,13 +3,23 @@ from time import sleep
 import board
 import digitalio
 
-print("press the button!")
+'''
+* TBD
+1. write and rotate log files
+2. Make the logger as a module
+3. write the button unit as a module
+4. write the led unit as a module
+5. Check write functions
+'''
+ 
+
+print("KOMOREBI LOG")
 
 
-button = digitalio.DigitalInOut(board.D4)
+button = digitalio.DigitalInOut(board.D23)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 
 while True:
     print(button.value)
-    sleep(2)
+    sleep(1)
