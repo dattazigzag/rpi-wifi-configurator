9a58a4df90404c231611c265ca64a8358782adb0
Saurabh Datta <hi@dattasaurabh.com>
2024-09-18 17:11:31 +0200

    webserver debug m,ode warning mititgation - 90% done

--- logger.py
+++ logger.py
@@ -2,21 +2,37 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 import os
 from datetime import datetime
-import shutil
+# import shutil
 
 class CustomTimedRotatingFileHandler(TimedRotatingFileHandler):
     def __init__(self, filename, when='h', interval=1, backupCount=0, encoding=None, delay=False, utc=False, atTime=None):
-        super().__init__(filename, when, interval, backupCount, encoding, delay, utc, atTime)
+        self.root_dir = os.path.dirname(os.path.abspath(__file__))
+        self.logs_dir = os.path.join(self.root_dir, "logs")
+        os.makedirs(self.logs_dir, exist_ok=True)
+        self.main_log_file = os.path.join(self.root_dir, filename)
+        super().__init__(self.main_log_file, when, interval, backupCount, encoding, delay, utc, atTime)
         self.initial_backup()
 
     def initial_backup(self):
-        if os.path.exists(self.baseFilename):
-            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-            backup_dir = os.path.join(os.path.dirname(self.baseFilename), "logs")
-            os.makedirs(backup_dir, exist_ok=True)
-            backup_file = os.path.join(backup_dir, f"log_{timestamp}.log")
-            shutil.copy2(self.baseFilename, backup_file)
-            os.remove(self.baseFilename)
+            if os.path.exists(self.main_log_file):
+                timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+                backup_file = os.path.join(self.logs_dir, f"log_{timestamp}.log")
+                os.rename(self.main_log_file, backup_file)  # Rename and move instead of copy
+
+    def doRollover(self):
+        if self.stream:
+            self.stream.close()
+            self.stream = None
+        
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        backup_file = os.path.join(self.logs_dir, f"log_{timestamp}.log")
+        
+        if os.path.exists(self.main_log_file):
+            os.rename(self.main_log_file, backup_file)
+        
+        if not self.delay:
+            self.stream = self._open()
+
 
 class Logger:
     def __init__(self, name, log_file='komorebi.log', level=logging.DEBUG):
@@ -32,11 +48,13 @@ class Logger:
         self.logger.addHandler(ch)
         
         # File handler (rotates every 24 hours)
-        log_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), log_file)
-        fh = CustomTimedRotatingFileHandler(log_path, when="D", interval=1, backupCount=30)
+        fh = CustomTimedRotatingFileHandler(log_file, when="D", interval=1, backupCount=0)
         fh.setLevel(level)
         fh.setFormatter(formatter)
         self.logger.addHandler(fh)
+
+        # Disable propagation to avoid duplicate logs
+        self.logger.propagate = False
 
     def debug(self, message):
         self.logger.debug(message)
@@ -55,3 +73,8 @@ class Logger:
 
 # Create a global logger instance
 logger = Logger('komorebi')
+
+# Capture Flask and Werkzeug logs
+werkzeug_logger = logging.getLogger('werkzeug')
+werkzeug_logger.setLevel(logging.ERROR)  # Only log errors from Werkzeug
+werkzeug_logger.addHandler(logger.logger.handlers[1])  # Add file handler
--- wifi_config/notes.py
+++ wifi_config/notes.py
@@ -1,31 +1,36 @@
+import os
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 from wifi_config.network_manager import NetworkManager
 import threading
 from logger import logger
 from time import sleep
-import os
+from werkzeug.serving import run_simple
+
+
+# Set environment variables to suppress warnings
+os.environ['WERKZEUG_RUN_MAIN'] = 'true'
+os.environ['FLASK_ENV'] = 'production'
+
 
 # ------------------------------------------- #
-# ************* Global Variables ************ #
+# * Global Variables ** #
 # ------------------------------------------- #
 
 server_running = False
 server_thread = None
 PORT = 80
 
-
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-
 @socketio.on('connect')
 def test_connect():
-    logger.debug("[web_server.py][Status] Client connected")
+    logger.info("[web_server.py][Status] Client connected")
 
 @socketio.on('disconnect')
 def test_disconnect():
-    logger.debug("[web_server.py][Status] Client disconnected")
+    logger.info("[web_server.py][Status] Client disconnected")
 
 @app.route('/')
 def index():
@@ -39,22 +44,19 @@ def serve_socketio_js():
 def serve_image(filename):
     return app.send_static_file(f'images/{filename}')
 
-
-
-
 @socketio.on('connect_wifi')
 def handle_connect_wifi(data):
     ssid = data['ssid']
     password = data['password']
 
     success, message = NetworkManager.connect_to_wifi(ssid, password)
-    
+
     logger.debug(f"[web_server.py][Status] Connection success: {success}")
     logger.debug(f"[web_server.py][Status] Connection message: {message}")
-    
+
     if success:
         ip = NetworkManager.get_current_ip()
-        logger.debug(f'[web_server.py][Result] The current IP is: {ip}')
+        logger.info(f'[web_server.py][Result] The current IP is: {ip}')
         socketio.emit('connection_result', {'success': True, 'ip': ip})
         # Start a new thread to shut down the server
         threading.Thread(target=shutdown_server).start()
@@ -67,6 +69,7 @@ def run_server():
     global server_running
     server_running = True
     socketio.run(app, host='0.0.0.0', port=PORT, debug=False, log_output=False, allow_unsafe_werkzeug=True)
+    run_simple('0.0.0.0', PORT, app, use_reloader=False, use_debugger=False)
 
 
 def stop_server():
@@ -76,6 +79,7 @@ def stop_server():
         logger.info("[web_server.py][Status] Stopping web server...")
         socketio.stop()
         logger.info("[web_server.py][Result] Web server stopped.")
+
 
 def shutdown_server():
     stop_server()
